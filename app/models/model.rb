aea23b566c729adf69c6b3edf7ac15958608ccc5
Taylor Otey <taylorotey5@gmail.com>
2018-08-14 20:04:28 +0000

    automatically backed up by learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,16 @@
-class Model
-  # Replace with Dog clas
+class Dog
+  attra_accessor :name, :breed, :age
+  DOGS = []
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+    DOGS << self
+   
 end
+
+def self.assert_select_all
+  DOGS
+end
+end
+
--- config.ru
+++ config.ru
@@ -1,3 +1,7 @@
 require_relative './config/environment'
 
 run ApplicationController
+
+
+
+
